48229735be9ae5acde493b6ce496c801b941075b
MarcoFonti <theemporiumg@gmail.com>
2024-01-20 16:39:15 +0100

    add snack 2

--- js/date.js
+++ js/date.js
@@ -7,6 +7,7 @@ console.log('JS DATI OK');
 2- USO IL METODO REDUCE PER CALCOLARE LE ZUCCHINE
 3- STAMPO
 */
+/*
 
 // ARRAY DI 10 OGETTI
 const arrayZuchinis = 
@@ -21,5 +22,30 @@ const arrayZuchinis =
     { variety: 'Zucchina Lunga', weight: 170, length: 26 },
     { variety: 'Zucchina Mini', weight: 90, length: 15 },
     { variety: 'Zucchina Trombetta', weight: 180, length: 28 }
+];
+
+*/
+
+// ! SCALETTA NACK 2
+/*
+1- CREO ARRAY DI 10 OGGETTI
+2- USO IL METODO REDUCE PER SEPARARE LE DIMISIONI DELLE ZUCCHINE
+3- CALCOLO CON IL REDUCE IL PESO DEI DUE GRUPPI
+3- STAMPO
+*/
+
+// ARRAY DI 10 OGETTI
+const arrayZuchinis = 
+[
+    { variety: 'Zucchina Verde', weight: 150, length: 9 },
+    { variety: 'Zucchina Gialla', weight: 120, length: 18 },
+    { variety: 'Zucchina Striata', weight: 130, length: 22 },
+    { variety: 'Zucchina Romanesca', weight: 140, length: 21 },
+    { variety: 'Zucchina Bianca', weight: 125, length: 5 },
+    { variety: 'Zucchina Nera', weight: 160, length: 24 },
+    { variety: 'Zucchina Tonda', weight: 110, length: 17 },
+    { variety: 'Zucchina Lunga', weight: 170, length: 12 },
+    { variety: 'Zucchina Mini', weight: 90, length: 15 },
+    { variety: 'Zucchina Trombetta', weight: 180, length: 10 }
 ];
 
--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ console.log('JS OK');
 2- USO IL METODO REDUCE PER CALCOLARE LE ZUCCHINE
 3- STAMPO
 */
+/*
 
 // METODO FOREACH
 const arrayWeight = arrayZuchinis.reduce((acc, element) => {
@@ -15,3 +16,46 @@ const arrayWeight = arrayZuchinis.reduce((acc, element) => {
 
 //STAMPO
 console.log('PESO TOTALE: ' + arrayWeight);
+
+*/
+
+// ! SCALETTA NACK 2
+/*
+1- CREO ARRAY DI 10 OGGETTI
+2- USO IL METODO REDUCE PER SEPARARE LE DIMISIONI DELLE ZUCCHINE
+3- CALCOLO CON IL REDUCE IL PESO DEI DUE GRUPPI
+3- STAMPO
+*/
+
+// CREO NUOVO ARRAY DI OGETTI PER SEPARARE LE DUE LUNGHEZZE
+const arrayLenght = arrayZuchinis.reduce((acc, element) => {
+    if(element.length <= 15) {
+        acc['inferior'].push(element);
+    } else{
+        acc['superior'].push(element);
+    }
+    
+    return acc;
+}, {inferior: [], superior: []})
+
+
+// STAMPO
+console.log(arrayZuchinis);
+console.log(arrayLenght);
+
+
+// PRENDO ARRAYLENGHT E CALCOLO IL PESO DELLE DUE CHIAVI
+
+// INFERIORE A 15CM
+const arrayInferior = arrayLenght.inferior.reduce((acc, element) => {
+    return acc + element.weight
+}, 0)
+
+console.log('PESO ZUCCHINE INFERIORE A 15CM: ' + arrayInferior);
+
+// SUPERIORI A 15CM
+const arraySuperior = arrayLenght.superior.reduce((acc, element) => {
+    return acc + element.weight
+}, 0)
+
+console.log('PESO ZUCCHINE SUPERIORE A 15CM: ' + arraySuperior);
