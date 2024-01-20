c39e4ac2bd40d9e79ae935a37de7f090e44de29d
MarcoFonti <theemporiumg@gmail.com>
2024-01-20 15:58:28 +0100

    add snack 1

--- js/date.js
+++ js/date.js
@@ -1,2 +1,25 @@
 // CHECK
 console.log('JS DATI OK');
+
+// ! SCALETTA NACK 1 
+/*
+1- CREO ARRAY DI 10 OGGETTI
+2- USO IL METODO REDUCE PER CALCOLARE LE ZUCCHINE
+3- STAMPO
+*/
+
+// ARRAY DI 10 OGETTI
+const arrayZuchinis = 
+[
+    { variety: 'Zucchina Verde', weight: 150, length: 20 },
+    { variety: 'Zucchina Gialla', weight: 120, length: 18 },
+    { variety: 'Zucchina Striata', weight: 130, length: 22 },
+    { variety: 'Zucchina Romanesca', weight: 140, length: 21 },
+    { variety: 'Zucchina Bianca', weight: 125, length: 19 },
+    { variety: 'Zucchina Nera', weight: 160, length: 24 },
+    { variety: 'Zucchina Tonda', weight: 110, length: 17 },
+    { variety: 'Zucchina Lunga', weight: 170, length: 26 },
+    { variety: 'Zucchina Mini', weight: 90, length: 15 },
+    { variety: 'Zucchina Trombetta', weight: 180, length: 28 }
+];
+
--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 // CHECK
 console.log('JS OK');
+
+// ! SCALETTA NACK 1 
+/*
+1- CREO ARRAY DI 10 OGGETTI
+2- USO IL METODO REDUCE PER CALCOLARE LE ZUCCHINE
+3- STAMPO
+*/
+
+// METODO FOREACH
+const arrayWeight = arrayZuchinis.reduce((acc, element) => {
+    return acc + element.weight
+}, 0)
+
+//STAMPO
+console.log('PESO TOTALE: ' + arrayWeight);
